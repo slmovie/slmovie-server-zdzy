0abba80f817f28259941a7e9aee110f2e9c39fab
Bao Jun <baojun.cz@gmail.com>
2020-04-10 23:02:59 +0800

    chore: local server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express"
 import compression from "compression"
 const router = require("./router")
-
 const app = express()
+
+app.all("*", function (req, res, next) {
+    res.set({
+      "Access-Control-Allow-origin": "*",
+      "Access-Control-Allow-Headers": "X-Requested-With",
+      "Access-Control-Allow-Methods": "GET"
+    });
+    next();
+});
+  
 app.use(compression())
 app.use(router)
 
