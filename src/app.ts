1970beb5907845648b5c10b205bf3a92428f8d11
Bao Jun <baojun.cz@gmail.com>
2020-04-13 20:54:50 +0800

    chore: web data

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,16 @@ const router = require("./router")
 const app = express()
 
 app.all("*", function (req, res, next) {
-    res.set({
-      "Access-Control-Allow-origin": "*",
-      "Access-Control-Allow-Headers": "X-Requested-With",
-      "Access-Control-Allow-Methods": "GET"
-    });
-    next();
+  res.set({
+    "Access-Control-Allow-origin": "*",
+    "Access-Control-Allow-Headers": "X-Requested-With",
+    "Access-Control-Allow-Methods": "GET"
+  });
+  next();
 });
-  
+
 app.use(compression())
+app.use(express.static("web"));
 app.use(router)
 
 app.listen(3010);
